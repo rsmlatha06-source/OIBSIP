9fb38e599b8f3af0835bfcde5d84e2d2768975ad
Rentala Sri Madhulatha <rsmlatha06@gmail.com>
2026-07-16 20:30:16 +0530

    Added basic BMI calculator

--- Task-2_BMI_Calculator/bmi_calculator.py
+++ Task-2_BMI_Calculator/bmi_calculator.py
@@ -1,1 +1,9 @@
 # BMI Calculator Project
+# BMI Calculator
+
+weight = float(input("Enter your weight (kg): "))
+height = float(input("Enter your height (m): "))
+
+bmi = weight / (height ** 2)
+
+print("Your BMI is:", round(bmi, 2))
