5b20b4342b4079af8bb8625ba4e03f05c0c28da3
Rentala Sri Madhulatha <rsmlatha06@gmail.com>
2026-08-11 12:10:13 +0530

    Completed Task 4 Beginner Weather App

---
+++ Task 4 – Weather App/weather_app.py
@@ -0,0 +1,56 @@
+import requests
+
+API_KEY = "d426d02c2d92e19e9a8b30b694679f29"
+
+city = input("Enter city name: ").strip()
+
+if city == "":
+    print("Error: City name cannot be empty.")
+
+else:
+    url = (
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?q={city}&appid={API_KEY}&units=metric"
+    )
+
+    try:
+        response = requests.get(url, timeout=5)
+
+        data = response.json()
+
+        if response.status_code == 200:
+
+            temperature_c = data["main"]["temp"]
+            temperature_f = (temperature_c * 9 / 5) + 32
+
+            humidity = data["main"]["humidity"]
+
+            weather = data["weather"][0]["description"]
+
+            wind_speed = data["wind"]["speed"]
+
+            print("\n========== Weather Report ==========")
+            print(f"City: {city.title()}")
+            print(f"Temperature: {temperature_c:.2f} °C")
+            print(f"Temperature: {temperature_f:.2f} °F")
+            print(f"Humidity: {humidity}%")
+            print(f"Weather: {weather.title()}")
+            print(f"Wind Speed: {wind_speed} m/s")
+
+        elif response.status_code == 404:
+            print("Error: City not found.")
+
+        elif response.status_code == 401:
+            print("Error: Invalid API key.")
+
+        else:
+            print("Error:", data.get("message", "Something went wrong."))
+
+    except requests.exceptions.Timeout:
+        print("Error: Request timed out.")
+
+    except requests.exceptions.ConnectionError:
+        print("Error: No Internet connection.")
+
+    except Exception as e:
+        print("Unexpected Error:", e)
